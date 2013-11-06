49d487c481b832ea70b9c9391f6f3e164c12788d
John Clem <johnnyclem@gmail.com>
2013-11-05 17:30:54 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ApiPopsiclCom::Application.config.secret_key_base = '572fc45811be707939444f7c32ce826379cb3b101f600caacf5ccab30ec507dcee625d7a5d2e1a12c21c89d10ba874b6e017ca63b9619a54d8d085b85eb9f332'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ApiPopsiclCom::Application.config.session_store :cookie_store, key: '_api_popsicl_com_session'
